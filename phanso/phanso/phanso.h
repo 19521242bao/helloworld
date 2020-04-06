33cd89a6360f9d95cdf4a3e9a8f3124b273264b5
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-04-06 18:26:42 +0700

    Create phanso.h

---
+++ phanso/phanso/phanso.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class phanso
+{
+private:
+	int tuso;
+	int mauso;
+public:
+	
+	void Nhap();
+	void Xuat();
+	void RutGon();
+	float DinhGT();
+	phanso CongPS(phanso a);
+	phanso TruPS(phanso a);
+	phanso NhanPS(phanso a);
+	phanso ChiaPS(phanso a);
+};
+
+
