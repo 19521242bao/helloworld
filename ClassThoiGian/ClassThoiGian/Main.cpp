6346c7076ded71eb83ae9cb3f08de164e08b483e
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-04-15 22:43:28 +0700

    Create Main.cpp

---
+++ ClassThoiGian/ClassThoiGian/Main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "Time.h"
+using namespace std;
+int main()
+{
+	Time a, b, c;
+	a.Nhap();
+	b.Nhap();
+	/*a.Display();
+	cout << endl;
+	cout << a.getHr()<<endl;
+	cout << a.getMin()<<endl;
+	cout << a.getSec()<<endl;*/
+	/*int x, y, z;
+	cin >> x >> y >> z;*/
+	/*a.setTime(x, y, z);
+	a.Display();*/
+	cout<<a.SoSanh(b);
+
+	/*a.TangTime(b);
+	a.Display();
+	a.GiamTime(b);
+	a.Display();*/
+
+}
