3d531843c7b6c970511b93d70b1224a4932cc103
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-04-20 10:51:49 +0700

    Create DaGiac.cpp

---
+++ DaGiac/DaGiac.cpp
@@ -0,0 +1,56 @@
+﻿// DaGiac.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "DaGiac.h"
+#include "ToaDo.h"
+using namespace std;
+
+void DaGiac::Nhap() {
+	cout << "Nhap so luong diem cu da giac:";
+	//n>=4;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		a[i].Nhap();
+	}
+}
+void DaGiac::Xuat() {
+	for (int i = 0; i < n; i++)
+		a[i].Xuat();
+}
+DaGiac::DaGiac()
+{
+	int n = 4;// mặc định số điểm là 4
+	for (int i = 0; i < n; i++)
+		a[i].setToaDo(0, 0);
+}
+DaGiac::DaGiac(const DaGiac& x)
+{
+	n = x.n;
+	for (int i = 0; i < n; i++)
+		a[i] = x.a[i];
+}
+
+void DaGiac::SetDaGiac(const DaGiac& x)
+{
+	n = x.n;
+	n = x.n;
+	for (int i = 0; i < n; i++)
+		a[i] = x.a[i];
+}
+void DaGiac::TinhTien(int x, int y)
+{
+	for (int i = 0; i < n; i++)
+		a[i].TinhTien(x, y);
+}
+void DaGiac::Size(float k)
+{
+	for (int i = 0; i < n; i++)
+		a[i].Size(k);
+}
+void DaGiac::Quay(float rad) {
+	for (int i = 0; i < n; i++)
+		a[i].Quay(rad);
+}
