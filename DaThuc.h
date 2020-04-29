c70c77a0b0aa66fd3ba4af9d93218392774ee582
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-04-29 23:52:05 +0700

    Create DaThuc.h

---
+++ DaThuc.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+class DaThuc
+{
+private:
+	int bac;
+	double* heso;
+public:
+	DaThuc();
+	DaThuc(const DaThuc&);
+	friend istream& operator>>(istream&, DaThuc&);
+	friend ostream& operator<<(ostream&, DaThuc&);
+	bool operator==(const DaThuc&);
+	bool operator>(const DaThuc&);
+	bool operator<(const DaThuc&);
+	DaThuc operator+(const DaThuc&);
+	DaThuc operator-(const DaThuc&);
+	DaThuc operator*(const DaThuc&);
+	DaThuc derivative();
+	DaThuc privitive(int c);// c là hệ số tự do
+};
