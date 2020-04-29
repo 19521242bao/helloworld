073a4885e26332d1ff060f54ddf9e910d40578fb
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-04-29 22:52:59 +0700

    Create DaThuc.h

---
+++ DaThuc/DaThuc.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+class DaThuc
+{
+private:
+	int n;
+	// so bac da thuc
+	float a[100];
+	//he so bậc của đa thức
+public:
+	DaThuc();
+	DaThuc(const DaThuc&);
+	void Nhap();
+	void Xuat();
+	void SetDaThuc(const DaThuc&);
+	int GetHS(int x);//a là bậc của hệ số muốn lấy
+	DaThuc CongDaThuc(const DaThuc&);
+	DaThuc TruDaThuc(const DaThuc&);
+	DaThuc NhanDaThuc(const DaThuc&);
+	DaThuc DaoHam();
+	DaThuc NgHam(int c);
+
+
+};
+
