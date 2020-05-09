d552c7369ce5a3202b078680471218060b65e918
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-05-09 12:26:33 +0700

    Create main.cpp

---
+++ sophuc1/sophuc1/main.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+#include "sophuc.h"
+using namespace std;
+int main()
+{
+	sophuc a, b, c;
+	cin >> a >> b;
+	if (a == b)
+		cout << "bang nhau";
+	else
+		cout << "khac nhau";s
+}
